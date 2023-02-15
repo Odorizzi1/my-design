75ba41b5afc7bf4e8721aaf84384397444de194a
jonathan <jonathan.odorizzi@clinicorp.com>
2023-02-14 22:36:38 -0300

    [Task - 3] Criação do TextField
    criação do TextField conforme cores primarias

---
+++ src/components/TextField.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function BasicTextFields() {
+    return (
+
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+
+    );
+}
--- src/pages/loginScreen.js
+++ src/pages/loginScreen.js
@@ -3,24 +3,29 @@ import NavBar from "../components/NavBar";
 import ButtonPrimary from "../components/ButtonPrimary";
 import ButtonOutlined from "../components/ButtonOutlined";
 import ButtonFlat from "../components/ButtonFlat";
+import { TextField } from "@mui/material";
+import BasicTextFields from "../components/TextField";
 
 const LoginScreen = () => {
     return (
         <div>
-          <NavBar />
-        <div>
-               <ButtonPrimary
-                label="Primary Button"
-            />
-            <ButtonOutlined
-                label="Button Outlined"
-            />
+            <NavBar />
+            <div>
+                <ButtonPrimary
+                    label="Primary Button"
+                />
+                <ButtonOutlined
+                    label="Button Outlined"
+                />
 
-            <ButtonFlat
-            label = "Button Flat"
-            />
-        </div>
-       
+                <ButtonFlat
+                    label="Button Flat"
+                />
+                <BasicTextFields />
+
+
+            </div>
+
         </div>
 
     )
