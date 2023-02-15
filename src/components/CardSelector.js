f3365dbbbcaa47dca15e099348532f6a865bc368
jonathan <jonathan.odorizzi@clinicorp.com>
2023-02-14 21:39:14 -0300

    [Task - 1] - Remoção de componentes
    Com a necessidade de criar um novo padrão de componentes, optei em começar a criar o design system e com isso iniciar a substituição dos componentes criados

--- src/components/CardSelector.js
+++ src/components/CardSelector.js
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import Modal from "../pages/Modal";
+// import React, { useState } from "react";
+// import Modal from "../pages/Modal";
 
-import ButtonIcon from "./ButtonIcon";
+// import ButtonIcon from "./ButtonIcon";
 
 
-const CardSelector = ({ titleTask, onClickDelete, onClickEdit, titleModal, infoCard }) => {
-  const [modalOpen, setModalOpen] = useState(false)
-  return (
-    <>
-      <Modal
-        show={modalOpen}
-        closeModal={() => setModalOpen(false)}
-        children={infoCard}
-      >
+// const CardSelector = ({ titleTask, onClickDelete, onClickEdit, titleModal, infoCard }) => {
+//   const [modalOpen, setModalOpen] = useState(false)
+//   return (
+//     <>
+//       <Modal
+//         show={modalOpen}
+//         closeModal={() => setModalOpen(false)}
+//         children={infoCard}
+//       >
       
-      </Modal>
-      <div className="content-insert__itens">
-        <span style={{ marginLeft: "5px", display: "flex", alignItems: "center", fontSize: "15px" }}>{titleTask}</span>
-        <div className="content-insert__itens--buton_icon">
+//       </Modal>
+//       <div className="content-insert__itens">
+//         <span style={{ marginLeft: "5px", display: "flex", alignItems: "center", fontSize: "15px" }}>{titleTask}</span>
+//         <div className="content-insert__itens--buton_icon">
 
-          <ButtonIcon
-            icon="edit"
-            onClick={() => setModalOpen(true)}
-          />
-          <ButtonIcon
-            icon="delete"
-            onClick={onClickDelete}
-          />
+//           <ButtonIcon
+//             icon="edit"
+//             onClick={() => setModalOpen(true)}
+//           />
+//           <ButtonIcon
+//             icon="delete"
+//             onClick={onClickDelete}
+//           />
 
-        </div>
-      </div>
+//         </div>
+//       </div>
 
-    </>
-  )
-}
-export default CardSelector;
+//     </>
+//   )
+// }
+// export default CardSelector;
--- src/pages/initialScreen.js
+++ src/pages/initialScreen.js
@@ -1,85 +1,85 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios'
-import CardSelector from "../components/CardSelector";
-import TextField from "../components/TextField";
-import Button from "../components/Button";
-import { utilsHelper } from "../utils/utilsHelper";
-import Tooltip from "../components/Tooltip";
+// import React, { useEffect, useState } from "react";
+// import axios from 'axios'
+// import CardSelector from "../components/CardSelector";
+// import TextField from "../components/TextField";
+// import Button from "../components/Button";
+// import { utilsHelper } from "../utils/utilsHelper";
+// import Tooltip from "../components/Tooltip";
 
-// import { Help } from "../utils/help";
+// // import { Help } from "../utils/help";
 
 
-const InitialScreen = () => {
+// const InitialScreen = () => {
 
-  const [useTask, setUseTask] = useState('')
-  const [listTask, setListTask] = useState([])
-  const [error, setError] = useState("")
-  useEffect(() => {
-    executeGet()
+//   const [useTask, setUseTask] = useState('')
+//   const [listTask, setListTask] = useState([])
+//   const [error, setError] = useState("")
+//   useEffect(() => {
+//     executeGet()
 
-  }, [])
+//   }, [])
 
-  function executeGet() {
-    utilsHelper.beGet('http://localhost:3001/task', (res) => setListTask(res.data))
-  }
+//   function executeGet() {
+//     utilsHelper.beGet('http://localhost:3001/task', (res) => setListTask(res.data))
+//   }
 
-  function createTask() {
-    if (useTask.length === 0) {
-      setError("É necessário inserir um texto!")
-    } else {
-      utilsHelper.bePost('http://localhost:3001/taskCreate', { title: useTask }, () => {
-        executeGet()
-        setUseTask("")
-      })
-    }
-  }
+//   function createTask() {
+//     if (useTask.length === 0) {
+//       setError("É necessário inserir um texto!")
+//     } else {
+//       utilsHelper.bePost('http://localhost:3001/taskCreate', { title: useTask }, () => {
+//         executeGet()
+//         setUseTask("")
+//       })
+//     }
+//   }
 
-  function deleteTask(_id) {
-    utilsHelper.beDelete(`http://localhost:3001/deleteTask/${_id}`, {}, () => executeGet())
-  }
+//   function deleteTask(_id) {
+//     utilsHelper.beDelete(`http://localhost:3001/deleteTask/${_id}`, {}, () => executeGet())
+//   }
 
-  function onChangeTextField(params) {
-    setUseTask(params)
-    console.log(params)
-  }
+//   function onChangeTextField(params) {
+//     setUseTask(params)
+//     console.log(params)
+//   }
 
-  return (
-    <div className="initial-screen">
-      <div className="containter-insert__itens">
-        <TextField
-          errorText={error}
-          label="Insira uma tarefa!"
-          onChange={(params) => onChangeTextField(params)}
-          value={useTask}
-        />
-        <Tooltip message={"teste"}>
-        <Button
-          disabled={useTask.length == 0}
-          className={"button-add__style"}
-          leftIcon="add_circle"
-          label={"Criar"}
-          onClick={() =>
-            createTask()}
-        />
+//   return (
+//     <div className="initial-screen">
+//       <div className="containter-insert__itens">
+//         <TextField
+//           errorText={error}
+//           label="Insira uma tarefa!"
+//           onChange={(params) => onChangeTextField(params)}
+//           value={useTask}
+//         />
+//         <Tooltip message={"teste"}>
+//         <Button
+//           disabled={useTask.length == 0}
+//           className={"button-add__style"}
+//           leftIcon="add_circle"
+//           label={"Criar"}
+//           onClick={() =>
+//             createTask()}
+//         />
 
-        </Tooltip>
-      </div>
+//         </Tooltip>
+//       </div>
 
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-        {listTask?.map(res => {
-          return <CardSelector
-            titleTask={res.title}
-            onClickDelete={() => deleteTask(res._id)}
-            onClickEdit={() => console.log()}
-            titleModal={res.title}
-            infoCard={"Observações a ser criadas"}
+//       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+//         {listTask?.map(res => {
+//           return <CardSelector
+//             titleTask={res.title}
+//             onClickDelete={() => deleteTask(res._id)}
+//             onClickEdit={() => console.log()}
+//             titleModal={res.title}
+//             infoCard={"Observações a ser criadas"}
 
-          />
-        })
-        }
-        {/* <button onClick={()=> teste()}>TESTEEEEEE</button> */}
-      </div>
-    </div>
-  )
-}
-export default InitialScreen;
+//           />
+//         })
+//         }
+//         {/* <button onClick={()=> teste()}>TESTEEEEEE</button> */}
+//       </div>
+//     </div>
+//   )
+// }
+// export default InitialScreen;
